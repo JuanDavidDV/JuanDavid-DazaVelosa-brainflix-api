9261c17352fb59998657655ce2c3159ad4e09627
JuanDavidDV <juandadazav@gmail.com>
2024-09-15 16:10:04 -0400

    successfully connection between back end and front end

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import fs from "fs";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 const readAllVideosData = () => {
     const videosData = fs.readFileSync("./data/videos.json");
