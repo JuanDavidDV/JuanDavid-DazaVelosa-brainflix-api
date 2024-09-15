ef2b14f0ee5faedaf1524efec57ef0e033d497ff
JuanDavidDV <juandadazav@gmail.com>
2024-09-15 16:05:48 -0400

    installed cors and start developing default home page endpoint

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from "express";
-import axios from "axios";
+import fs from "fs";
 
 const app = express();
+
+const readAllVideosData = () => {
+    const videosData = fs.readFileSync("./data/videos.json");
+    const videosDataParsed = JSON.parse(videosData);
+    return videosDataParsed;
+}
+
+
+app.get("/", (req, res) => {
+    const allVideosData = readAllVideosData();
+    res.json(allVideosData);
+})
 
 app.listen(8080, () => {
     console.log("Server is running on http://localhost:8080 ");
