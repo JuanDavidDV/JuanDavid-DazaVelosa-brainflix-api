131d08e20e4bb0df3c6fba17279ac21cbf2732f1
JuanDavidDV <juandadazav@gmail.com>
2024-09-17 15:45:52 -0400

    route endpoint function created for dynamic endpoint

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 import express from "express";
-import fs from "fs";
+import videosRoutes from "./routes/videos.js";
 import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 
-const readAllVideosData = () => {
-    const videosData = fs.readFileSync("./data/videos.json");
-    const videosDataParsed = JSON.parse(videosData);
-    return videosDataParsed;
-}
-
-
-app.get("/videos/:videoId", (req, res) => {
-    
-    const allVideosData = readAllVideosData();
-    console.log(allVideosData);
-    res.json(allVideosData);
-})
 
 app.listen(8080, () => {
     console.log("Server is running on http://localhost:8080");
--- routes/videos.js
+++ routes/videos.js
@@ -1,2 +1,18 @@
-import fs from "fs";
 import express from "express";
+import fs from "fs";
+
+const router = express.Router(); //Enables Router use
+
+const readAllVideosData = () => {
+    const videosData = fs.readFileSync("../data/videos.json");
+    const videosDataParse = JSON.parse(videosData);
+    return videosDataParse;
+}
+
+router.get("/:videoId", (req, res) => {
+    const allVideosData = readAllVideosData();
+    console.log(allVideosData);
+    return res.json(allVideosData);
+});
+
+export default router;
