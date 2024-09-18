145f422b2319da4190959e79067fc1881e25bd0b
JuanDavidDV <juandadazav@gmail.com>
2024-09-17 20:56:10 -0400

    router created in video.js

--- routes/videos.js
+++ routes/videos.js
@@ -3,20 +3,19 @@ import fs from "fs";
 
 const router = express.Router(); //Enables Router use
 
+const videosData = fs.readFileSync("./data/videos.json");
+const videosDataParse = JSON.parse(videosData);
+
 const readAllVideosData = (videoId) => {
-    const videosData = fs.readFileSync("./data/videos.json");
-    const videosDataParse = JSON.parse(videosData);
-    const selectVideo = videosDataParse.find((video) => video.id === videoId);
+    const selectVideo = videosDataParse.find((video) => video.id === videoId);  //DO I NEED TO USE THE FIND FUNCTION IN MY BACK END OR FRONT END?
     console.log(selectVideo);
     return selectVideo;
 }
 
 router.get("/:videoId", (req, res) => {
     const videoId = req.params.videoId;
-    console.log(videoId);
     const allVideosData = readAllVideosData(videoId);
-    //console.log(allVideosData);
-    return res.json(allVideosData);
+    res.status(200).json(allVideosData);
 });
 
 export default router;
