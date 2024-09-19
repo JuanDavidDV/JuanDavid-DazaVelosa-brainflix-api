684111a9b807af756738a46fb2ce6b08a81973fa
JuanDavidDV <juandadazav@gmail.com>
2024-09-18 21:28:50 -0400

    add back end link cors origin

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from "express";
+import "dotenv/config";
 import videosRoutes from "./routes/videos.js";
 import cors from "cors";
 
 const app = express();
+const { BACKEND_URL, CORS_ORIGIN } = process.env;
+const PORT = process.env.PORT || 5051;
 
-app.use(cors());
+console.log(PORT);
+console.log(BACKEND_URL);
+console.log(CORS_ORIGIN)
+
+app.use(cors({ CORS_ORIGIN })); 
 app.use("/videos", videosRoutes);
 
 
-app.listen(8080, () => {
-    console.log("Server is running on http://localhost:8080");
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on ${BACKEND_URL}${PORT}`);
+})   
