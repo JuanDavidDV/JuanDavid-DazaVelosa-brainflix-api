c029cc752fe23f2262dc5e20f92bf2739a5f5bb0
JuanDavidDV <juandadazav@gmail.com>
2024-09-18 20:12:39 -0400

    add back .env file

--- routes/videos.js
+++ routes/videos.js
@@ -3,10 +3,9 @@ import fs from "fs";
 
 const router = express.Router(); //Enables Router use
 
-const videosData = fs.readFileSync("./data/videos.json");
-const videosDataParse = JSON.parse(videosData);
-
 const readAllVideosData = (videoId) => {
+    const videosData = fs.readFileSync("./data/videos.json");
+    const videosDataParse = JSON.parse(videosData);
     const selectVideo = videosDataParse.find((video) => video.id === videoId);  //DO I NEED TO USE THE FIND FUNCTION IN MY BACK END OR FRONT END?
     console.log(selectVideo);
     return selectVideo;
