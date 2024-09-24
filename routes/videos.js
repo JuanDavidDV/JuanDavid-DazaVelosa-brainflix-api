11227a237d2e4b49c55fedb979f8a0528b8680e1
JuanDavidDV <juandadazav@gmail.com>
2024-09-23 22:31:05 -0400

    post function and object started to be built

--- routes/videos.js
+++ routes/videos.js
@@ -8,19 +8,28 @@ router.use(express.json());
 const videosData = fs.readFileSync("./data/videos.json");   //Retrieves data from videos.json file
 const videosDataParse = JSON.parse(videosData);     //Parsed video data
 
+const newVideo = {
+    "id": uuidv4(),
+    
+}
+
 router.get("/:id", (req, res) => {
     const videoId = req.params.id;
     const selectVideo = videosDataParse.find((video) => video.id === videoId);
     res.status(200).json(selectVideo);
 });
 
-router.get("/", (req, res) => {
-    res.status(200).json(videosDataParse.map((video) => ({
-        id: video.id,
-        title: video.title,
-        channel: video.channel,
-        image: video.image
-    })));
-});
+router.route("/")
+    .get((req, res) => {
+        res.status(200).json(videosDataParse.map((video) => ({
+            id: video.id,
+            title: video.title,
+            channel: video.channel,
+            image: video.image
+        })));
+    })
+    .post((req, res) => {
+
+    });
 
 export default router;
