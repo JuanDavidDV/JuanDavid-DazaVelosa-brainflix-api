795503e777ed1673c7a682275c524a4ae4fd6eb0
JuanDavidDV <juandadazav@gmail.com>
2024-09-29 23:34:18 -0400

    comments added

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ const { BACKEND_URL, CORS_ORIGIN } = process.env;
 const PORT = process.env.PORT || 5051;
 
 app.use(cors({ CORS_ORIGIN })); 
-app.use(express.json());
+app.use(express.json());    //Middleware to parse JSON on body-parser
 app.use(express.static("./public"));    // creates static asset for public folder containing the images and video
-app.use("/videos", videosRoutes);
-
-
+app.use("/videos", videosRoutes);   //Incorporates routes for videos
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${BACKEND_URL}${PORT}`);
--- routes/videos.js
+++ routes/videos.js
@@ -1,16 +1,18 @@
 import express from "express";
 import fs from "fs";
-import { v4 as uuidv4} from "uuid";
+import { v4 as uuidv4} from "uuid"; //Generates random ID
 
 const router = express.Router(); //Enables Router use
 
 const videosData = fs.readFileSync("./data/videos.json");   //Retrieves data from videos.json file
 const videosDataParse = JSON.parse(videosData);     //Parsed video data
 
+//Function created to modify JSON video data 
 const syncVideosData = (data) => {
     fs.writeFileSync("./data/videos.json", JSON.stringify(data));
 }
 
+//Gets video information according to video Id
 router.get("/:id", (req, res) => {
     const videoId = req.params.id;
     const selectVideo = videosDataParse.find((video) => video.id === videoId);
@@ -24,21 +26,21 @@ router.get("/:id", (req, res) => {
 });
 
 router.route("/")
-    .get((req, res) => {
-        res.status(200).json(videosDataParse.map((video) => ({
+    .get((req, res) => {    //Gets Next Video response  
+        res.status(200).json(videosDataParse.map((video) => ({ //Creates an object to send response with specific keys and values 
             id: video.id,
             title: video.title,
             channel: video.channel,
             image: video.image
         })));
     })
-    .post((req, res) => {
+    .post((req, res) => {   //Posts new video uploaded by user into JSON video data file
         const { title, imageUrl, description } = req.body;
-        const newVideo = {
+        const newVideo = {  //Creates object containing all video information
             id: uuidv4(),   //Generates new ID for new post video
             title: title,
             channel: "Mohan Muruge",
-            image: imageUrl ? imageUrl : "http://localhost:8080/images/Upload-video-preview.jpg",
+            image: imageUrl ? imageUrl : "http://localhost:8080/images/Upload-video-preview.jpg",   //If user does not provide image file, it will provide default upload video image
             description: description,
             views: "0",
             likes: "0",
@@ -48,33 +50,35 @@ router.route("/")
             comments: []
         };
 
-        videosDataParse.push(newVideo);
+        videosDataParse.push(newVideo); //Adds new object into array JSON video data
         syncVideosData(videosDataParse);
         res.status(200).json(newVideo);
     });
 
+//Posts a comment to respective video Id
 router.post("/:id/comments", (req, res) => {
     const videoId = req.params.id;
     const { name, comment } = req.body;
-    const currentVideo = videosDataParse.find((video) => video.id === videoId);
+    const currentVideo = videosDataParse.find((video) => video.id === videoId); //Selects current Video Id
     const newComment = {
-        id: uuidv4(),
+        id: uuidv4(),   //Generates random comment ID
         name: name,
         comment: comment,
         likes: 0,
         timestamp: Date.now()
     };
 
-    currentVideo.comments.push(newComment);
+    currentVideo.comments.push(newComment); //Adds comment to into comment array
     syncVideosData(videosDataParse);
     res.status(200).json(newComment);
 });
 
+//Deletes comment on specific video according to video ID
  router.delete("/:videoId/comments/:commentId", (req, res) => { 
      const videoId = req.params.videoId;
      const commentId = req.params.commentId;
 
-     const currentVideo = videosDataParse.find((video) => video.id === videoId);
+     const currentVideo = videosDataParse.find((video) => video.id === videoId);    //Selects current Video Id
      const currentCommentDelete = currentVideo.comments.splice(
         currentVideo.comments.findIndex((comment) => comment.id === commentId), 1) [0]; //[0] returns the body of the comment deleted
     
@@ -82,9 +86,10 @@ router.post("/:id/comments", (req, res) => {
     res.status(200).json(currentCommentDelete);
  });
 
+ //Increments video Likes count according to video id
  router.put("/:videoId/likes", (req, res) => {
     const videoId = req.params.videoId;
-    const currentVideo = videosDataParse.find((video) => video.id === videoId);
+    const currentVideo = videosDataParse.find((video) => video.id === videoId); //Selects current Video Id
     currentVideo.likes = (parseInt(currentVideo.likes.replaceAll(",", "")) + 1).toLocaleString("en-us");
     syncVideosData(videosDataParse);
     res.status(200).json(currentVideo.likes);
